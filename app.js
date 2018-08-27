a76df34cf5b78cfe7fa0cce1e0bd913ace3cf98d
Hoang Tran <hayden.tran.d@gmail.com>
2018-08-27 18:12:51 +0700

    config cors to app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require('cors');
 
 const articleModel = require("./models/article");
 const articleRoute = require("./routes/article")(articleModel);
@@ -18,6 +19,20 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:8000'];
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+}));
 
 app.use("/api", articleRoute);
 app.get("/", (req, res) => res.send("Hello World!"));
